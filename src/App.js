cff6ef8650a4a2ca0df20109f4b53bf19ca95db2
Raymond Doetjes <rdoetjes@phonax.com>
2023-03-07 16:59:54 +0100

    faro beginnings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import Board from './components/Board'
 import './App.css';
 
+const BB=1;
+const BF=2;
+const WB=3;
+const WF=4;
+
 function App() {
+  const n = 5;
+  
+  const [board, setBoard] = useState(Array.from({length: n},()=> Array.from({length: n}, () => 0)));
+
+  function create_board(size) {
+    let board = Array.from({length: size},()=> Array.from({length: size}, () => 0));
+    const blackPieces=[BB, BB, BF, BB, BB];
+    const whitePieces=[WB, WB, WF, WB, WB];
+
+    for(let i=0; i<size; i++){
+      for(let j=0; j<size; j++){
+        if (i===0){
+          board[i][j] = blackPieces[j];
+          continue;
+        }
+        
+        if (i===4){
+          board[i][j] = whitePieces[j];
+          continue;
+        }
+      }
+    }
+    setBoard(board);
+  }
+
+  useEffect(() => {
+    create_board(n);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='center'>
+      <Board board={board} size={n}/>
     </div>
   );
 }
---
+++ src/components/Board.js
@@ -0,0 +1,27 @@
+import { React } from 'react';
+
+
+function renderImages(board, size){
+    const game = []
+    console.log(board)
+    for(let i=0; i<size; i++){
+        for(let j=0; j<size; j++){
+            console.log(board[i][j]);
+            game.push(<img data-id={i+","+j} key={i+","+j} src={"images/"+board[i][j]+".png"} alt={board[i][j]}></img>);
+        }
+    }
+    return game;
+}
+
+function Board({board, size}) {
+    return(
+    <div className='board_enclosure'>
+        <div className='title'>KING'S VALLEY</div>
+        <div className="board">
+            {renderImages(board, size)}
+        </div>
+    </div>
+    );
+}
+
+export default Board;
